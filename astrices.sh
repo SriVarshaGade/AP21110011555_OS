23c7d48adb12220d1152df25b9d3cd5ae0758bb1
SriVarshaGade <117621518+SriVarshaGade@users.noreply.github.com>
2023-03-09 00:06:07 -0500

    Create astrices.sh

---
+++ astrices.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Output the username with asterisks
+echo "****"
+echo "Username:"
+echo "$USER"
+echo "****"
+
+# Output the current time and date with asterisks
+echo "****"
+echo "Current time and date:"
+date
+echo "****"
+
+# Output the logged-in users with asterisks
+echo "****"
+echo "Logged-in users:"
+who
+echo "****"
